ac5670d6c046d7562c63615af125605a6784d205
Isabella Santos <isabella.santos@gocase.com.br>
2017-07-07 09:42:45 -0300

    Change class name

--- test/test_konduto_navigation.rb
+++ test/test_konduto_navigation.rb
@@ -1,11 +1,11 @@
 require 'minitest/autorun'
 require 'factory_girl'
 
-class TestKondutoNavigationInfo < Minitest::Test
+class TestKondutoNavigation < Minitest::Test
   include FactoryGirl::Syntax::Methods
 
   def test_serialization
-    navigation_info = build(:konduto_navigation_info)
+    navigation_info = build(:konduto_navigation)
     navigation_infoJSON = load_resource('navigation.json')
 
     assert_equal navigation_info.to_hash, navigation_infoJSON, 'serialization falied'
